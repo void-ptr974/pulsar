de0987e8719beb1875af12b4e925e60bd554bc78
void-ptr974 <void-ptr974@protonmail.com>
2026-06-26 23:25:27 +0800

    [test][broker] Add replicator read limit test callback

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentReplicator.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/persistent/PersistentReplicator.java
@@ -125,6 +125,7 @@ public abstract class PersistentReplicator extends AbstractReplicator
     protected ReasonOfWaitForCursorRewinding reasonOfWaitForCursorRewinding = null;
 
     protected final LinkedList<InFlightTask> inFlightTasks = new LinkedList<>();
+    private ReadTaskStateCallback readTaskStateCallback;
 
     public PersistentReplicator(String localCluster, PersistentTopic localTopic, ManagedCursor cursor,
                                 String remoteCluster, String remoteTopic,
@@ -222,6 +223,15 @@ public abstract class PersistentReplicator extends AbstractReplicator
     private record ReadLimits(int messages, long bytes) {
         public boolean isReadable() {
             return messages > 0 && bytes > 0;
+        }
+    }
+
+    @VisibleForTesting
+    interface ReadTaskStateCallback {
+        default void readLimitsCalculated(int readPermits, int messagesToRead, long maxBytesToRead) {
+        }
+
+        default void inFlightTaskCreated(InFlightTask inFlightTask) {
         }
     }
 
@@ -818,6 +828,13 @@ public abstract class PersistentReplicator extends AbstractReplicator
         return cursor;
     }
 
+    @VisibleForTesting
+    void setReadTaskStateCallback(ReadTaskStateCallback callback) {
+        synchronized (inFlightTasks) {
+            readTaskStateCallback = callback;
+        }
+    }
+
     @Data
     protected static class InFlightTask {
         Position readPos;
@@ -931,6 +948,9 @@ public abstract class PersistentReplicator extends AbstractReplicator
             }
 
             ReadLimits readLimits = getReadLimits(permits);
+            if (readTaskStateCallback != null) {
+                readTaskStateCallback.readLimitsCalculated(permits, readLimits.messages, readLimits.bytes);
+            }
 
             if (!readLimits.isReadable()) {
                 // no rate limiter permits from rate limit
@@ -941,8 +961,12 @@ public abstract class PersistentReplicator extends AbstractReplicator
                 return null;
             }
 
-            return createOrRecycleInFlightTaskIntoQueue(cursor.getReadPosition(), readLimits.messages,
+            InFlightTask task = createOrRecycleInFlightTaskIntoQueue(cursor.getReadPosition(), readLimits.messages,
                     readLimits.bytes);
+            if (readTaskStateCallback != null) {
+                readTaskStateCallback.inFlightTaskCreated(task);
+            }
+            return task;
         }
     }
 
